5bb996ae01292652ffc6116239c27434578dc847
masha-a-m <escritoramaree@gmail.com>
2025-07-16 22:59:33 +0800

    added signup

--- js/main.js
+++ js/main.js
@@ -24,3 +24,16 @@ document.addEventListener('DOMContentLoaded', function () {
       window.location.href = "signup.html"; // Make sure this file exists
     }
   }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const togglePassword = document.getElementById('togglePassword');
+  const passwordInput = document.getElementById('password');
+
+  togglePassword.addEventListener('click', function () {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+  });
+});
